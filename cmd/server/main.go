abde10f0dbdc243207e2e6613bc719ea75b0aef1
Sofi <Sofi.Rubizova@yandex.ru>
2023-10-03 17:13:24 +0300

    iter 9 is completed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Sofja96/go-metrics.git/internal/handlers"
 	"github.com/Sofja96/go-metrics.git/internal/server/config"
 	"github.com/Sofja96/go-metrics.git/internal/storage"
@@ -14,17 +13,23 @@ func main() {
 	s := storage.NewMemStorage(c.StoreInterval, c.FilePath, c.Restore)
 	if c.FilePath != "" {
 		if c.Restore {
+			//	storage.LoadStorageFromFile(s, c.FilePath)
 			err := storage.LoadStorageFromFile(s, c.FilePath)
 			if err != nil {
-				panic(err)
+				log.Print(err)
 			}
 		}
 		if c.StoreInterval != 0 {
-			go storage.Dump(s, c.FilePath, c.StoreInterval)
+			go func() {
+				err := storage.Dump(s, c.FilePath, c.StoreInterval)
+				if err != nil {
+					log.Print(err)
+				}
+			}()
 		}
 	}
 	e := handlers.CreateServer(s)
-	fmt.Println("Running server on", c.Address)
+	log.Println("Running server on", c.Address)
 	err := e.Start(c.Address)
 	if err != nil {
 		log.Fatal(err)
