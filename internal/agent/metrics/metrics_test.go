b7b4edfbe983f4ce0a0e3153460e56b51717983e
Sofi <Sofi.Rubizova@yandex.ru>
2024-12-10 23:50:27 +0300

    refactoring iter20

--- internal/agent/metrics/metrics_test.go
+++ internal/agent/metrics/metrics_test.go
@@ -49,30 +49,3 @@ func TestGetPSMetrics(t *testing.T) {
 	assert.Equal(t, ValuesGauge["FreeMemory"], float64(memStat.Free))
 	assert.Equal(t, ValuesGauge["CPUutilization1"], float64(cpuPercent[0]))
 }
-
-func TestConcurrency(t *testing.T) {
-	ValuesGauge = make(map[string]float64)
-	ValuesCounter = make(map[string]int64)
-	var Mu sync.Mutex
-
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		GetMetrics()
-	}()
-
-	go func() {
-		defer wg.Done()
-		GetPSMetrics()
-	}()
-
-	wg.Wait()
-
-	Mu.Lock()
-	defer Mu.Unlock()
-	assert.GreaterOrEqual(t, ValuesCounter["PollCount"], int64(1))
-	assert.Contains(t, ValuesGauge, "Alloc")
-	assert.Contains(t, ValuesGauge, "TotalMemory")
-}
